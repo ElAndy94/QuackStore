e376a3268c00cee8e0b44cd68bd098dfc8026d69
Mahmoud Shneb <mahmouds@apadmi.com>
2022-10-12 15:28:13 +0100

    feat: added basket state to basket page

--- pages/basket/index.tsx
+++ pages/basket/index.tsx
@@ -2,9 +2,17 @@ import React, { useState } from 'react';
 import Layout from '../../components/Layout';
 import Image from 'next/image';
 import Button from '../../components/UI/Buttons';
+import useBasket from '../../store/basket';
+import LargeCard from '../../components/ProductsView/LargeCard';
 
 const Basket = () => {
-  const [selectedQuantity, setSelectedQuantity] = useState<number>(0);
+  const {
+    addToBasket,
+    basketProducts,
+    totalPrice,
+    removeFromBasket,
+    updateProductQuantity,
+  } = useBasket();
 
   return (
     <Layout
@@ -23,63 +31,18 @@ const Basket = () => {
             </p>
           </div>
           <ul className="mt-10">
-            <li>
-              <article className="flex w-full">
-                <figure className="w-1/2 h-full">
-                  <Image
-                    src="/assets/images/shoes.jpeg"
-                    alt="shoes"
-                    width="400"
-                    height="400"
+            {basketProducts.map(product => {
+              return (
+                <li key={product.sys.id}>
+                  <LargeCard
+                    product={product}
+                    onRemove={() => removeFromBasket(product)}
+                    onIncrease={() => updateProductQuantity(product, '+')}
+                    onDecrease={() => updateProductQuantity(product, '-')}
                   />
-                </figure>
-                <div className="px-10 w-full flex flex-col justify-between">
-                  <div className="flex flex-col gap-2">
-                    <div className="flex justify-between items-center">
-                      <h5 className="text-lg font-semibold">Nike Competition Shoes</h5>
-                      <p className="text-primary font-medium text-lg">£{100}</p>
-                    </div>
-                    <p className="text-granite-gray">Men&apos;s Footwear</p>
-                    <p className="text-granite-gray">
-                      Size: <span className="text-primary font-medium">{9}</span>
-                    </p>
-                    <p className="text-granite-gray">
-                      Estimated delivery date:
-                      <span className="text-primary font-medium"> 12-16 April</span>
-                    </p>
-                  </div>
-                  <div className="flex justify-between">
-                    <div className="flex flex-row">
-                      <button
-                        type="button"
-                        className="bg-grey-200 w-12 h-12 border-y border-l"
-                        onClick={() => {
-                          selectedQuantity > 0 &&
-                            setSelectedQuantity(selectedQuantity - 1);
-                        }}
-                      >
-                        -
-                      </button>
-                      <p className="flex items-center justify-center w-14 h-12 border-y">
-                        {selectedQuantity}
-                      </p>
-                      <button
-                        type="button"
-                        className="bg-grey-200 w-12 h-12 border-y border-r"
-                        onClick={() => {
-                          setSelectedQuantity(selectedQuantity + 1);
-                        }}
-                      >
-                        +
-                      </button>
-                    </div>
-                    <button type="button" className="text-magenta font-medium">
-                      Remove
-                    </button>
-                  </div>
-                </div>
-              </article>
-            </li>
+                </li>
+              );
+            })}
           </ul>
         </section>
         <section className="border p-4">
@@ -117,7 +80,7 @@ const Basket = () => {
             </div>
             <div className="flex justify-between">
               <p>Total</p>
-              <p>£{140}</p>
+              <p>£{totalPrice}</p>
             </div>
           </article>
           <article className="flex flex-col justify-between gap-5 p-6">
